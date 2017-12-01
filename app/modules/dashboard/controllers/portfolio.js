4d2b8588c442e999107c57f59db60a4095d3b6bf
abhinav ralhan <abhinav.ralhan1@gmail.com>
2017-12-01 12:16:32 +0530

    final app

---
+++ app/modules/dashboard/controllers/portfolio.js
@@ -0,0 +1,40 @@
+﻿
+dashboard.controller("PortfolioController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash',
+function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
+    var vm = this;
+    
+
+    vm.portfolioData = [
+        {
+            image: "housewife",
+            title: "Housewife"
+        },
+        {
+            image: "student",
+            title: "Student"
+        },{
+            image: "professional",
+            title: "Professional"
+        }
+    ];
+
+    vm.temp = [
+        {
+            n: 1,
+            statee: false
+        },
+        {
+            n: 2,
+            statee: false
+        },
+        {
+            n: 3,
+            statee: false
+        }
+    ];
+
+    console.log("coming to Portfolio controller");
+
+
+}]);
+
---
+++ public/app/modules/dashboard/controllers/about.js
@@ -0,0 +1,8 @@
+dashboard.controller("AboutController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash',
+function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
+        var vm = this;
+
+        console.log("coming to Portfolio controller");
+
+    }]);
+
